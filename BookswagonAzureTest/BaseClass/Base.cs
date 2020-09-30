75b5ab74af81cebeb2988de00f91a8821dea8362
pratikshatamadalge <pratikshatamadalge21@gmail.com>
2020-09-30 12:29:00 +0530

    Added:Extent Report and screenshot feature

--- BookswagonAzureTest/BaseClass/Base.cs
+++ BookswagonAzureTest/BaseClass/Base.cs
@@ -1,13 +1,23 @@
-﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Threading;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.MarkupUtils;
 using NUnit.Framework;
-using System;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using BookswagonAzureTest.ExtentReport;
+using BookswagonAzureTest.Utilities;
 
 namespace BookswagonAzureTest.BaseClass
 {
     public class Base
     {
         public IWebDriver driver;
+        public const string path = @"F:\VS\BookswagonAzureTest\BookswagonAzureTest\Screenshots";
+        public static ExtentReports extent = ReportManager.GetInstance();
+        public static ExtentTest test;
         [OneTimeSetUp]
         public void Setup()
         {
@@ -15,6 +25,36 @@ namespace BookswagonAzureTest.BaseClass
             options.AddArguments("start-maximized", "--disable-notifications");
             driver = new ChromeDriver(options);
             driver.Url = "https://www.bookswagon.com/login";
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            try
+            {
+                test = extent.CreateTest(TestContext.CurrentContext.Test.Name);
+                if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed)
+                {
+                    string path = Utility.TakeScreenshot(driver, TestContext.CurrentContext.Test.Name);
+                    test.Log(Status.Fail, "Test Failed");
+                    test.AddScreenCaptureFromPath(path);
+                    test.Fail(MarkupHelper.CreateLabel(TestContext.CurrentContext.Test.Name, ExtentColor.Red));
+                }
+                else if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Passed)
+                {
+                    test.Log(Status.Pass, "Test Sucessful");
+                    test.Pass(MarkupHelper.CreateLabel(TestContext.CurrentContext.Test.Name, ExtentColor.Green));
+                }
+            }
+            catch (Exception e)
+            {
+                
+                Console.Out.WriteLine(e.StackTrace);
+                Console.Out.WriteLine(e.Message);
+            }
+
+            Thread.Sleep(5000);
+            extent.Flush();
         }
 
         [OneTimeTearDown]
---
+++ BookswagonAzureTest/ExtentReport/ReportManager.cs
@@ -0,0 +1,29 @@
+﻿namespace BookswagonAzureTest.ExtentReport
+{
+    using AventStack.ExtentReports;
+    using AventStack.ExtentReports.Reporter;
+
+    class ReportManager
+    {
+        private static ExtentReports extent;
+        private static ExtentHtmlReporter htmlReporter;
+
+        /// <summary>
+        /// To get instance of extent report.
+        /// </summary>
+        /// <returns> extent class object.</returns>
+        public static ExtentReports GetInstance()
+        {
+            if (extent == null)
+            {
+                string reportPath = @"F:\VS\BookswagonAzureTest\BookswagonAzureTest\ExtentReport\index.html";
+                htmlReporter = new ExtentHtmlReporter(reportPath);
+                extent = new ExtentReports();
+                extent.AttachReporter(htmlReporter);
+                extent.AddSystemInfo("OS", "Windows 10");
+                extent.AddSystemInfo("Browser", "Chrome");
+            }
+            return extent;
+        }
+    }
+}
